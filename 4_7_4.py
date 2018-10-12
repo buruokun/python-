add0fcd767e20e2127f0d19ba0b709396675eafc
buruokun <44092551+buruokun@users.noreply.github.com>
2018-10-12 23:23:57 +0900

    Add files via upload

---
+++ 4_7_4.py
@@ -0,0 +1,4 @@
+def parrot(voltage,state='a stiff',action='voom'):
+  print("-- This parrot wouldn't ",action,end=' ')
+  print("if you put ",voltage,"volts through it.",end=' ')
+  print("E's",state,"!")
